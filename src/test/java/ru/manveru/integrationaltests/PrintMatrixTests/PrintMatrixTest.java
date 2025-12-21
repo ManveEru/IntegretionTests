c6b176b56de337af58adb44d6366a3746fc24a82
mihav <mihav@Amber>
2025-12-21 16:43:20 +0700

    Allure setup and

--- src/test/java/ru/manveru/integrationaltests/PrintMatrixTests/PrintMatrixTest.java
+++ src/test/java/ru/manveru/integrationaltests/PrintMatrixTests/PrintMatrixTest.java
@@ -1,8 +1,10 @@
 package ru.manveru.integrationaltests.PrintMatrixTests;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -34,10 +36,10 @@ public class PrintMatrixTest extends PrintMatrixTestBase{
         //prepareed in testDataProvider
         
         //Actions
-        List<String> response = sendRequest(requestParams);
+        List<String> response = sendRequestStep(requestParams);
 
         //Assertions
-        assertEquals(expectedString, response.get(0), testDescription);
+        Allure.step("Проверка ответа запроса", () -> assertEquals(expectedString, response.get(0), testDescription));
     }
   
     @Test
@@ -49,14 +51,16 @@ public class PrintMatrixTest extends PrintMatrixTestBase{
         Map<String, Integer> requestParams = Map.of("per_line", 20);
         
         //Actions
-        List<String> response = sendRequest(requestParams);
+        List<String> response = sendRequestStep(requestParams);
             
         //Assertions
-        assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20", response.get(0), "First stirng is incorrect");
-        assertEquals("21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40", response.get(1), "Second stirng is incorrect");
-        assertEquals("41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60", response.get(2), "Third stirng is incorrect");
-        assertEquals("61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80", response.get(3), "Fouth stirng is incorrect");
-        assertEquals("81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100", response.get(4), "Fifs stirng is incorrect");
+        Allure.step("Проверка ответа запроса", () -> assertAll(
+            () -> Allure.step("В первой строке цифры от 1 до 20", () -> assertEquals("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20", response.get(0))),
+            () -> Allure.step("Во второй строке цифры от 21 до 30", () -> assertEquals("21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40", response.get(1))),
+            () -> Allure.step("В третьей строке цифры от 41 до 60", () -> assertEquals("41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60", response.get(2))),
+            () -> Allure.step("В четвёртой строке цифры от 61 до 80", () -> assertEquals("61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80", response.get(3))),
+            () -> Allure.step("В пятой строке цифры от 81 до 100", () -> assertEquals("81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100", response.get(4)))
+        ));
     }
     
     @Test
@@ -69,19 +73,21 @@ public class PrintMatrixTest extends PrintMatrixTestBase{
         Map<String, Integer> requestParams = Map.of();
         
         //Actions
-        List<String> response = sendRequest(requestParams);
+        List<String> response = sendRequestStep(requestParams);
             
         //Assertions
-        assertEquals("1 2 3 4 5 6 7 8 9 10", response.get(0), "First stirng is incorrect");
-        assertEquals("11 12 13 14 15 16 17 18 19 20", response.get(1), "Second stirng is incorrect");
-        assertEquals("21 22 23 24 25 26 27 28 29 30", response.get(2), "Third stirng is incorrect");
-        assertEquals("31 32 33 34 35 36 37 38 39 40", response.get(3), "Fouth stirng is incorrect");
-        assertEquals("41 42 43 44 45 46 47 48 49 50", response.get(4), "Fifs stirng is incorrect");
-        assertEquals("51 52 53 54 55 56 57 58 59 60", response.get(5), "Sixth stirng is incorrect");
-        assertEquals("61 62 63 64 65 66 67 68 69 70", response.get(6), "Seventh stirng is incorrect");
-        assertEquals("71 72 73 74 75 76 77 78 79 80", response.get(7), "Eights stirng is incorrect");
-        assertEquals("81 82 83 84 85 86 87 88 89 90", response.get(8), "Nineth stirng is incorrect");
-        assertEquals("91 92 93 94 95 96 97 98 99 100", response.get(9), "Tenth stirng is incorrect");
+        Allure.step("Проверка ответа запроса", () -> assertAll(
+            () -> Allure.step("В первой строке цифры от 1 до 10", () -> assertEquals("1 2 3 4 5 6 7 8 9 10", response.get(0))),
+            () -> Allure.step("Во второй строке цифры от 11 до 20", () -> assertEquals("11 12 13 14 15 16 17 18 19 20", response.get(1))),
+            () -> Allure.step("В третьей строке цифры от 21 до 30", () -> assertEquals("21 22 23 24 25 26 27 28 29 30", response.get(2))),
+            () -> Allure.step("В четвёртой строке цифры от 31 до 40", () -> assertEquals("31 32 33 34 35 36 37 38 39 40", response.get(3))),
+            () -> Allure.step("В пятой строке цифры от 41 до 50", () -> assertEquals("41 42 43 44 45 46 47 48 49 50", response.get(4))),
+            () -> Allure.step("В шестой строке цифры от 51 до 60", () -> assertEquals("51 52 53 54 55 56 57 58 59 60", response.get(5))),
+            () -> Allure.step("В седьмой строке цифры от 61 до 70", () -> assertEquals("61 62 63 64 65 66 67 68 69 70", response.get(6))),
+            () -> Allure.step("В восьмой строке цифры от 71 до 80", () -> assertEquals("71 72 73 74 75 76 77 78 79 80", response.get(7))),
+            () -> Allure.step("В девятой строке цифры от 81 до 90", () -> assertEquals("81 82 83 84 85 86 87 88 89 90", response.get(8))),
+            () -> Allure.step("В десятой строке цифры от 91 до 100", () -> assertEquals("91 92 93 94 95 96 97 98 99 100", response.get(9)))
+                ));
     }
     
     @Test
@@ -96,13 +102,18 @@ public class PrintMatrixTest extends PrintMatrixTestBase{
                 "per_line", 3);
         
         //Actions
-        List<String> response = sendRequest(requestParams);
+        List<String> response = sendRequestStep(requestParams);
 
         //Assertions
-        assertAll(
-        () -> assertEquals("5 6 0", response.get(1), "Second is incorrect"),
-        () -> assertEquals(2, response.size(), "Line count is incorrect")
-        );
+        Allure.step("Проверка ответа запроса", () -> assertAll(
+        () -> Allure.step("Вторая строка дополнена нулями", () -> assertEquals("5 6 0", response.get(1))),
+        () -> Allure.step("В ответе 2 строки", () -> assertEquals(2, response.size()))
+        ));
+    }
+    
+    @Step("Отправка запроса")
+    private List<String> sendRequestStep(Map<String, Integer> requestParams){
+        return sendRequest(requestParams);
     }
     
     @DisplayName("Провайдер данных для параметризированных тестов")
